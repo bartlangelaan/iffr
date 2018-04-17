adc299fff2bbbff5671bce58c02ed91b4ee691e8
Bart Langelaan <langelaanbart@gmail.com>
2018-04-17 17:57:12 +0200

    style: add tslint, tslint-config-airbnb and prettier

--- src/api/router.ts
+++ src/api/router.ts
@@ -2,8 +2,10 @@ import * as Router from 'koa-router';
 
 const router = new Router();
 
-router.get('/', ctx => {ctx.body = {
-  status: 'OK',
-}});
+router.get('/', ctx => {
+  ctx.body = {
+    status: 'OK',
+  };
+});
 
 export default router;
