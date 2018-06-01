46ce7232db9419fddd9ac2fef9fbd6c4ffb126df
Bart Langelaan <langelaanbart@gmail.com>
2018-06-01 18:36:57 +0200

    feat(user): add support for requesting other users

--- src/api/user.ts
+++ src/api/user.ts
@@ -5,14 +5,26 @@ const router = new Router({
   prefix: '/user',
 });
 
-router.get('/', async ctx => {
-  const { userUuid } = ctx.state;
-  if (!userUuid) {
-    ctx.throw(403);
-    return;
+router.get('/:userId', async ctx => {
+  const { userUuid, permissions } = ctx.state;
+  let { userId } = ctx.params;
+
+  // If the user is requesting info about himself, ensure a user
+  // is actually connecting and has permission to do that.
+  if (userId === 'me') {
+    if (!userUuid || !permissions.includes('user.view')) {
+      ctx.throw(403);
+      return;
+    }
+    userId = userUuid;
+  } else if (!permissions.includes('users.view')) {
+    // If it isn't requesting itself, it needs permission to view all users. If
+    // that isn't allowed, return a 403 response.
+    throw ctx.throw(403);
   }
+
   ctx.body = {
-    user: await user.getUser(userUuid),
+    user: await user.getUser(userId),
   };
 });
 
