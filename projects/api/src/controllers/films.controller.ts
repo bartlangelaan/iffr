680d0cac18b1772400cf226710a6620a6b29385a
Bart Langelaan <langelaanbart@gmail.com>
2018-06-28 17:25:16 +0200

    feat(films): split up in 3 endpoints

--- projects/api/src/controllers/films.controller.ts
+++ projects/api/src/controllers/films.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { FilmsProvider } from '../providers/films';
+import { ApiUseTags } from '@nestjs/swagger';
 
 @Controller('films')
+@ApiUseTags('films')
 export class FilmsController {
   constructor(private readonly films: FilmsProvider) {}
 
@@ -9,13 +11,23 @@ export class FilmsController {
   async list(
     @Param('year', new ParseIntPipe())
     year: number,
-    @Query('extended') extended: boolean,
-    @Query('full') full: boolean,
   ) {
-    if (!full) {
-      return this.films.list(year, extended);
-    }
+    return this.films.list(year);
+  }
 
+  @Get('/list/:year/extended')
+  async listExtended(
+    @Param('year', new ParseIntPipe())
+    year: number,
+  ) {
+    return this.films.list(year, true);
+  }
+
+  @Get('/list/:year/full')
+  async listFull(
+    @Param('year', new ParseIntPipe())
+    year: number,
+  ) {
     const list = await this.films.list(year);
     return Promise.all(list.map(f => this.films.get(f.id)));
   }
