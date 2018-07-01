446d87df41f7e9fa29f4e1c84836d5fe7d2f03d4
Bart Langelaan <langelaanbart@gmail.com>
2018-07-01 02:19:46 +0200

    feat(matcher): style the guide screens

--- projects/matcher/src/app/app.component.tsx
+++ projects/matcher/src/app/app.component.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import store, { Screen } from './app.store';
 import Guide1Screen from '../screens/guide/guide1.component';
 import Guide2Screen from '../screens/guide/guide2.component';
@@ -10,10 +11,17 @@ import MainScreen from '../screens/main/main.screen';
 @observer
 export default class App extends React.Component {
   render() {
-    if (store.screen === Screen.Guide1) return <Guide1Screen />;
-    if (store.screen === Screen.Guide2) return <Guide2Screen />;
-    if (store.screen === Screen.Guide3) return <Guide3Screen />;
-    if (store.screen === Screen.Login) return <LoginScreen />;
-    return <MainScreen />;
+    // tslint:disable-next-line:variable-name
+    let ScreenComponent: React.ComponentType = MainScreen;
+    if (store.screen === Screen.Guide1) ScreenComponent = Guide1Screen;
+    if (store.screen === Screen.Guide2) ScreenComponent = Guide2Screen;
+    if (store.screen === Screen.Guide3) ScreenComponent = Guide3Screen;
+    if (store.screen === Screen.Login) ScreenComponent = LoginScreen;
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <ScreenComponent />
+      </React.Fragment>
+    );
   }
 }
--- projects/matcher/src/screens/guide/base.component.tsx
+++ projects/matcher/src/screens/guide/base.component.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import store, { Screen } from '../../app/app.store';
 
 @observer
@@ -9,11 +11,39 @@ export default class GuideBase extends React.Component<P> {
   };
   render() {
     return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <p>{this.props.subtitle}</p>
-        <button onClick={this.continueButtonClicked}>Next ></button>
-      </div>
+      <Grid
+        container
+        style={{ height: '100%', textAlign: 'center' }}
+        direction="row"
+        alignItems="center"
+        justify="center"
+      >
+        <Grid
+          item
+          style={{ height: '90%', maxHeight: 400, width: '75%', maxWidth: 800 }}
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="space-between"
+        >
+          <Grid item>
+            <h1>{this.props.title}</h1>
+          </Grid>
+          <Grid item>
+            <p>{this.props.subtitle}</p>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.continueButtonClicked}
+            >
+              Doorgaan
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
     );
   }
 }
